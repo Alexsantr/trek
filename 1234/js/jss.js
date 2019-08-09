2499ace307bed4ed141e793a4fd772c721812ce4
Alexsantr <50623230+Alexsantr@users.noreply.github.com>
2019-08-09 18:08:47 +0300

    Add files via upload

---
+++ 1234/js/jss.js
@@ -0,0 +1,23 @@
+function viv(){
+document.getElementById("black").style.opacity ="1.0" ;
+document.getElementById("black").style.fontSize ="50px" ;
+
+};
+function  myFunction(){
+    var it=document.getElementById("black")
+    it.style.fontSize="50px";
+   
+    
+    
+    
+    
+  
+ 
+};
+
+function showsl(){
+var uy=document.getElementById("tera")
+
+uy.style.opacity ="1.0";
+
+};
