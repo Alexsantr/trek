627f947ef84698ce648b2fefd498dcca929ffa7e
Alexsantr <50623230+Alexsantr@users.noreply.github.com>
2019-08-13 17:58:30 +0300

    Add files via upload

--- 1234/js/jss.js
+++ 1234/js/jss.js
@@ -29,7 +29,48 @@ function det(){
        
         };
 
-        function wer(){
-            var t=document.getElementsByTagName("li")
-            t.style.transform="translateX(10px)";
-        }
+
+const responsives=document.querySelectorAll('.responsive');
+  for (let i = 0; i < responsives.length; i++) {
+     const  responsive = responsives[i];
+     responsive.addEventListener('mousemove',startRotate);
+     
+     responsive.addEventListener('mouseout',stopRotate);
+    }
+function startRotate(event){
+    const gallery=this.querySelector('.gallery');
+  
+    
+    gallery.style. height='500px';
+    gallery.style. width='500px';
+ 
+   
+    
+}
+
+function stopRotate(event){
+    const gallery=this.querySelector('.gallery');
+    gallery.style. height='none';
+    gallery.style. width='none';
+}
+
+
+  // const halfHeight = gallery.offsetHeight/2;
+    // gallery.style.transform='rotateX('+-(event.offsetY -halfHeight)/5 +'deg) rotateY(' +(event.offsetX -halfHeight)/5 +'deg)   ';
+
+
+// console.log(li);
+    //     function wer(t){
+    //        t=document.getElementsByTagName("li")
+    //         t.style.transform="translateY(-10px)"
+         
+    //     }
+    //     function q(y){
+    //   y=document.getElementsByTagName("li")
+    //    y.style.transform="translateY(0px)"
+    //     }
+// function ll()
+// {
+//     var r=document.getElementsByTagName(body)
+//     r.style.transform="0";
+// }
